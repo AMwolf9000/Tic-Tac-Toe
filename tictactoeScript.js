03e4058dd183b325b2328b265f6194b94e7d2280
AMwolf9000 <169104276+AMwolf9000@users.noreply.github.com>
2024-07-28 16:48:21 -0400

    Add files via upload
    added a line that goes through the winning 3 marks.

--- tictactoeScript.js
+++ tictactoeScript.js
@@ -1,8 +1,9 @@
 'use strict';
 let boxes = document.getElementsByClassName('box');
-let t, m, boardData;
-t = '';
-m = 0;
+let t, m, boardData, winMark;
+t = ''; // current turn
+m = 0; // moves made
+winMark = document.getElementsByTagName('svg')[0];
 boardData = ['0','0','0','0','0','0','0','0','0'];
 function start(x) {
 	if (t === '') {
@@ -27,21 +28,20 @@ function reset() {
 	boardData = ['0','0','0','0','0','0','0','0','0'];
 	document.getElementsByTagName('h1')[0].innerHTML = 'No one\'s turn';
 	t = '';
-	m = 0
+	m = 0;
+	winMark.innerHTML = '';
 }
 function addMark() {
 	if (t != '') {
 		if (this.innerHTML === '') {
-			this.innerHTML = t.toUpperCase()
+			this.innerHTML = t.toUpperCase();
 			boardData[this.id] = t;
 			m++;
-			if (checkWin() === true) {
-				return;
-			}
+			checkWin();
 			if (t === 'x') {
-				t = 'o';
+				t = 'o'
 			} else {
-				t = 'x';
+				t = 'x'
 			}
 			document.getElementsByTagName('h1')[0].innerHTML = `${t.toUpperCase()}'s turn`
 		}
@@ -49,31 +49,75 @@ function addMark() {
 }
 function checkWin() {
 	if (m >= 5) {
-		let b = boardData.join('');
-		if (/^(xxx|ooo)|^...(xxx|ooo)...$|(xxx|ooo)$/.test(b)) {
-			alert(`${t.toUpperCase()} horizontal win!`);
-			reset();
-			return true;
-		}
-		let b0 = b[0] + b[3] + b[6] + b[1] + b[4] + b[7] + b[2] + b[5] + b[8];
-		if (/^(xxx|ooo)|^...(xxx|ooo)...$|(xxx|ooo)$/.test(b0)) {
-			alert(`${t.toUpperCase()} vertical win!`);
-			reset();
-			return true;
-		}
-		let b1 = b[0] + b[4] + b[8] + b[2] + b[4] + b[6];
-		if (/^(xxx|ooo)|(xxx|ooo)$/.test(b1)) {
-			alert(`${t.toUpperCase()} diagonal win!`);
-			reset();
-			return true;
-		}
-		if (m === 9) {
-			alert('draw!');
-			reset();
-			return true;
-		}
+			let b = boardData.join('');
+			if (/^(xxx|ooo)/.test(b)) {
+				return endWinCheck('1');
+			}
+			if (/...(xxx|ooo).../.test(b)) {
+				return endWinCheck('2');
+			}
+			if (/(xxx|ooo)$/.test(b)) {
+				return endWinCheck('3');
+			}
+			if (/x..x..x..|o..o..o../.test(b)) {
+				return endWinCheck('4');
+			}
+			if (/.x..x..x.|.o..o..o./.test(b)) {
+				return endWinCheck('5');
+			}
+			if (/..x..x..x|..o..o..o/.test(b)) {
+				return endWinCheck('6');
+			}
+			if (/x...x...x|o...o...o/.test(b)) {
+				return endWinCheck('7');
+			}
+			if (/..x.x.x..|..o.o.o../.test(b)) {
+				return endWinCheck('8');
+			}
+
+			if (m === 9) {
+				alert('draw!');
+				reset();
+				return true;
+			}
 	}
 	return false;
+}
+function endWinCheck(x) {
+	let y;
+	if (x == '1') {
+		 winMark.innerHTML = '<line x1="0" y1="75" x2="500" y2="75" style="stroke:black;stroke-width:15">'
+	}
+	if (x == '2') {
+		 winMark.innerHTML = '<line x1="0" y1="250" x2="500" y2="250" style="stroke:black;stroke-width:15">'
+	}
+	if (x == '3') {
+		 winMark.innerHTML = '<line x1="0" y1="425" x2="500" y2="425" style="stroke:black;stroke-width:15">'
+	}
+	if (x == '4') {
+		 winMark.innerHTML = '<line x1="75" y1="0" x2="75" y2="500" style="stroke:black;stroke-width:15">'
+	}
+	if (x == '5') {
+		 winMark.innerHTML = '<line x1="250" y1="0" x2="250" y2="500" style="stroke:black;stroke-width:15">'
+	}
+	if (x == '6') {
+		 winMark.innerHTML = '<line x1="425" y1="0" x2="425" y2="500" style="stroke:black;stroke-width:15">'
+	}
+	if (x == '7') {
+		 winMark.innerHTML = '<line x1="0" y1="0" x2="500" y2="500" style="stroke:black;stroke-width:15">'
+	}
+	if (x == '8') {
+		 winMark.innerHTML = '<line x1="0" y1="500" x2="500" y2="0" style="stroke:black;stroke-width:15">'
+	}
+	setTimeout(() => { 
+		if (t == 'x') {
+			y = 'o'
+		} else {
+			y = 'x'
+		}
+		alert(`${y.toUpperCase()} Won!!`);
+		reset()
+	}, 50)
 }
 for (let i = 0; i < boxes.length; i++) {
 	boxes[i].addEventListener('click', addMark)
